370949438a9f64dda77ea214949319a56e42d71c
Kapil <kapilbhadu001@gmail.com>
2026-06-02 11:52:12 +0530

    Initialize exploration.py, create cleaner.py, clean.csv saved

---
+++ cleaner.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+df = pd.read_csv('data/raw/Sample - Superstore.csv',encoding='latin-1')
+
+pd.set_option('display.max_columns', None)
+
+new_header = list(df.columns)
+
+for i in range(len(new_header)):
+    new_header[i] = new_header[i].lower()
+    new_header[i] = new_header[i].strip()
+    new_header[i] = new_header[i].replace(' ','_').replace('-','_')
+
+df.columns = new_header
+
+df['order_date'] = pd.to_datetime(df['order_date'])
+df['ship_date'] = pd.to_datetime(df['ship_date'])
+
+df.to_csv('data/clean/clean.csv', index = False)
+
