ab2ac412f603814a98f06f30e3f126f23cd4eebe
Kapil <kapilbhadu001@gmail.com>
2026-06-02 12:10:11 +0530

    write setup code of db_loader.py

---
+++ db_loader.py
@@ -0,0 +1,13 @@
+import sqlite3
+import pandas as pd
+
+df = pd.read_csv('data/clean/clean.csv', parse_dates=['order_date', 'ship_date'])
+
+conn = sqlite3.connect('data/retail.db')
+
+df.to_sql('sales', conn, if_exists='replace', index=False)
+
+cursor = conn.cursor()
+cursor.execute('select count(*) from sales')
+
+print('Total Rows:',cursor.fetchone()[0])
